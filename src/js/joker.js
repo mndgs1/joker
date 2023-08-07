b1b9dbf2063884cc37d847fb2907a66e2db2ca91
mndgs1 <mindaugasbankauskas@gmail.com>
2023-08-07 10:58:05 +0200

    changed joker test to have a reject

--- src/js/joker.js
+++ src/js/joker.js
@@ -2,6 +2,12 @@ import joker from 'give-me-a-joke';
 
 export function dadJoke() {
   return new Promise(function (resolve, reject) {
-    joker.getRandomDadJoke(resolve);
+    try {
+      joker.getRandomDadJoke(function (joke) {
+        resolve(joke);
+      });
+    } catch (error) {
+      reject(error);
+    }
   });
 }
